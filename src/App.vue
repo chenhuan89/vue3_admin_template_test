21febe6e1e6ce29490f98c92c2a9ec7a49e0496a
chen <yqbxxc@foxmail.com>
2024-01-13 22:46:06 +0800

    test: 测试插件功能

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <h1>我是APP根组件</h1>
+    <h1>新增代码</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 </script>
 
 <style lang="scss" scoped></style>
