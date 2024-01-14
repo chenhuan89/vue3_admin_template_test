46d8ad373bdd760d60bccbceb56fad7b0796bef4
chen <yqbxxc@foxmail.com>
2024-01-14 16:08:59 +0800

    feat: 项目环境变量配置

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <div>
     <h1>我是APP根组件</h1>
     <h1>新增代码</h1>
+    <test></test>
   </div>
   <div>
     <el-button type="primary" size="default" :icon="Plus">Default</el-button>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts" name="t1">
 import { Plus } from '@element-plus/icons-vue'
+import test from '@/components/test.vue'
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>About</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { ref } from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
